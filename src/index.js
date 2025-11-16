e87017f25265c127f1b9237ec59b93c48d6622a8
Paras <paras.jain2@publicissapient.com>
2025-11-16 23:51:12 +0530

    redux updated

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
-import App from './components/App';
-import reducers from './reducers';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import axios from 'axios';
-import createSagaMiddleware from 'redux-saga';
-import rootSaga from './sagas';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { createRoot } from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import "./style.css";
+import App from "./components/App";
+import reducers from "./reducers";
+import rootSaga from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://rem-rest-api.herokuapp.com/api';
+axios.defaults.baseURL = "http://rem-rest-api.herokuapp.com/api";
 
-const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+const store = configureStore({
+  reducer: reducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: false, // we use saga instead of thunk
+    }).concat(sagaMiddleware),
+});
 
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
